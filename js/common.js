b097c29a5b1d4fc25a652e1da119584ec91ec46f
Duc Pham <minhducpham.it@gmail.com>
2018-03-10 10:14:24 +0700

    Duc

---
+++ js/common.js
@@ -0,0 +1,16 @@
+/*
+ * ---------------------------------------------------
+ * 1. Slide Carousel
+ * 2. Scroll to Top
+ * 3. Sticky Menu
+ * 4. Accordion has icon
+ * 5. Hover tag a show ul page Product
+ * 6. POPUP order a product - check on info Payment
+ * 7. Scroll News Item Tablet & Mobile
+ */
+
+  (function($){
+    "use strict";
+  /* ==================================================== */
+
+})(jQuery); // End of use strict
